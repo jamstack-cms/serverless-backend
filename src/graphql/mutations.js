6ac9520e2fb3f5bdffcc6a72697961543b32eafd
Dabit <dabit@8c85901b6e1c.ant.amazon.com>
2019-09-22 10:47:41 -0700

    initial commit

---
+++ src/graphql/mutations.js
@@ -0,0 +1,99 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const createComment = `mutation CreateComment($input: CreateCommentInput!) {
+  createComment(input: $input) {
+    id
+    message
+    createdBy
+    createdAt
+  }
+}
+`;
+export const updateComment = `mutation UpdateComment($input: UpdateCommentInput!) {
+  updateComment(input: $input) {
+    id
+    message
+    createdBy
+    createdAt
+  }
+}
+`;
+export const deleteComment = `mutation DeleteComment($input: DeleteCommentInput!) {
+  deleteComment(input: $input) {
+    id
+    message
+    createdBy
+    createdAt
+  }
+}
+`;
+export const createSettings = `mutation CreateSettings($input: CreateSettingsInput!) {
+  createSettings(input: $input) {
+    id
+    mode
+    categories
+    adminGroups
+  }
+}
+`;
+export const updateSettings = `mutation UpdateSettings($input: UpdateSettingsInput!) {
+  updateSettings(input: $input) {
+    id
+    mode
+    categories
+    adminGroups
+  }
+}
+`;
+export const deleteSettings = `mutation DeleteSettings($input: DeleteSettingsInput!) {
+  deleteSettings(input: $input) {
+    id
+    mode
+    categories
+    adminGroups
+  }
+}
+`;
+export const createPost = `mutation CreatePost($input: CreatePostInput!) {
+  createPost(input: $input) {
+    id
+    title
+    description
+    content
+    cover_image
+    createdAt
+    published
+    previewLink
+    category
+  }
+}
+`;
+export const updatePost = `mutation UpdatePost($input: UpdatePostInput!) {
+  updatePost(input: $input) {
+    id
+    title
+    description
+    content
+    cover_image
+    createdAt
+    published
+    previewLink
+    category
+  }
+}
+`;
+export const deletePost = `mutation DeletePost($input: DeletePostInput!) {
+  deletePost(input: $input) {
+    id
+    title
+    description
+    content
+    cover_image
+    createdAt
+    published
+    previewLink
+    category
+  }
+}
+`;
